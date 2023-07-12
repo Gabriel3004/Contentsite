3b9fbddbbb2ccf514a2a563bf36ac3e7a24d8674
Gael Bassong <133223868+GaelBass@users.noreply.github.com>
2023-07-12 13:18:01 +0200

    Delete src directory

--- src/client.jsx
+++
@@ -1,6 +0,0 @@
-import * as contentful from 'contentful'
-
-export const client = contentful.createClient({
-    space: process.env.REACT_APP_SPACE_ID,
-    accesToken: process.env.REACT_APP_ACCESS_TOKEN
-})
